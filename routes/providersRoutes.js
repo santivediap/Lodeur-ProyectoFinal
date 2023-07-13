5521331912689fee2aca7eabf1ba054486bc6886
santivediap <santivediap@gmail.com>
2023-07-13 02:19:40 +0200

    Create Providers routes

---
+++ routes/providersRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Rutas de providers
+const providersApiController = require("../controllers/providersControllers");
+const providersApiRouter = express.Router();
+
+providersApiRouter.post('/', providersApiController.createProvider);
+providersApiRouter.get('/', providersApiController.getProviders);
+providersApiRouter.get('/:name', providersApiController.getProvidersByName);
+providersApiRouter.delete('/', providersApiController.deleteProvider);
+
+module.exports = providersApiRouter;
