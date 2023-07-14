32d0d81ce56ce5ea3280e69a6b59e309e8218a1d
santivediap <santivediap@gmail.com>
2023-07-14 22:45:11 +0200

    Create Header and Footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>
+    <section className="footer-container">
+      <span>Footer</span>
+    </section>
+  </footer>;
+};
+
+export default Footer;
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from './NavBar'
+
+const Header = () => {
+  return <header>
+    <NavBar />
+  </header>;
+};
+
+export default Header;
