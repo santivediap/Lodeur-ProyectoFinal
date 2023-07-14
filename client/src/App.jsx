e8d55d980c8bca29696ae889f17e31b65a97d0be
santivediap <santivediap@gmail.com>
2023-07-14 22:47:01 +0200

    Create NavBar and implement app's structure into App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import Home from './components/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-      </Routes>
-    </Router>
+    <Header />
+    <Main />
+    <Footer />
     </>
   );
 }
---
+++ client/src/components/Header/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavBar = () => {
+  return <nav>
+    <img src="assets/lodeur_logo.png" alt="" />
+  </nav>;
+};
+
+export default NavBar;
