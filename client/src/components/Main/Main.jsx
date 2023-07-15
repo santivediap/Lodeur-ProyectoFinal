c20831769be3bd80137417b2fed59c062a4add12
santivediap <santivediap@gmail.com>
2023-07-15 17:45:59 +0200

    Use ProductsList

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Search from './Search';
+import ProductsList from "./ProductsList/ProductsList";
+import ProductsContext from '../../context/productsContext'
+import PageContext from '../../context/pageContext'
 
 const Main = () => {
+
+  const { productsList, setProductsList } = useContext(ProductsContext)
+  const { page, setPage } = useContext(PageContext)
+
   return <main>
-    <Search />
+    <Search setPage={setPage} setProducts={setProductsList} />
+    <ProductsList page={page} setPage={setPage} setProducts={setProductsList} productsList={productsList} />
   </main>;
 };
 
