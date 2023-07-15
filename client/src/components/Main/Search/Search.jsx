c6afdab70cc3cdbdff1935e13803f1bafd258e1c
santivediap <santivediap@gmail.com>
2023-07-15 17:41:50 +0200

    Added final Search structure with final styles

--- client/src/components/Main/Search/Search.jsx
+++ client/src/components/Main/Search/Search.jsx
@@ -1,42 +1,139 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from 'axios'
+import { ThreeDots } from  'react-loader-spinner'
+import LoadingContext from "../../../context/loadingContext";
 
-const Search = () => {
-  return <section className="search-container">
-    <h2>¡BUSCA TU PRODUCTO YA!</h2>
+const Search = ({ setProducts, setPage }) => {
 
-    <form className="search-form" action="#">
+  const [productInput, setProductInput] = useState("")
+  const [providerInput, setProviderInput] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
 
-      <label>
-        <span>Producto</span>
-        <input type="text" name="product" id="product" />
-      </label>
+  const { loading, setLoading } = useContext(LoadingContext)
 
-      <label>
-        <span>Fabricante</span>
-        <input type="text" name="provider" id="provider" />
-      </label>
+  const changeProductInput = (e) => {
+    setProductInput(e.target.value)
+  }
 
-      <label>
-        <span>Filtrar por</span>
-        <select name="sorttype" id="sorttype">
-          <option value="title">Titulo</option>
-          <option value="price">Precio</option>
-          <option value="relevance">Relevancia</option>
-        </select>
-      </label>
+  const changeProviderInput = (e) => {
+    setProviderInput(e.target.value)
+  }
 
-      <label>
-        <span>Ordenar de</span>
-        <select name="sort" id="sorttype">
-          <option value="desc">Mayor a menor</option>
-          <option value="asc">Menor a mayor</option>
-        </select>
-      </label>
+  const submitSearch = (e) => {
+    e.preventDefault()
 
-      <button type="submit">BUSCAR</button>
+    if(/[a-zA-Z0-9\s]+/gm.test(productInput) || /[a-zA-Z0-9\s]+/gm.test(providerInput)) {
+      if(productInput.length && providerInput.length) {
+        searchProducts({
+          product: productInput,
+          provider: providerInput,
+          sortType: e.target.sorttype.value,
+          sort: e.target.sort.value,
+          page: 1
+        })
+      } else {
+        if(productInput.length) {
+          searchProducts({
+            product: productInput,
+            sortType: e.target.sorttype.value,
+            sort: e.target.sort.value,
+            page: 1
+          })
+        }
 
-    </form>
-  </section>;
+        if(providerInput.length) {
+          searchProducts({
+            provider: providerInput,
+            sortType: e.target.sorttype.value,
+            sort: e.target.sort.value,
+            page: 1
+          })
+        }
+      }
+    } else {
+      newError("¡Debes introducir algo!")
+    }
+  }
+
+  // Generates custom error message below search input
+  const newError = (errMessage) => {
+    setErrorMessage(errMessage)
+
+    setTimeout(() => {
+      setErrorMessage("")
+    }, 3000);
+  }
+
+  const searchProducts = async (searchParams) => {
+    const { product, provider, sortType, sort, page } = searchParams
+
+    if(provider && product) {
+      setLoading(true)
+      const products = await axios.get(`/api/products?page=1&title=${product}&provider=${provider}&sorttype=${sortType}&sort=${sort}`)
+      setPage([page, `/api/products?page=1&title=${product}&provider=${provider}&sorttype=${sortType}&sort=${sort}`])
+      setLoading(false)
+      setProducts(products.data)
+    } else {
+      if(provider) {
+        setLoading(true)
+        const products = await axios.get(`/api/products?page=1&provider=${provider}&sorttype=${sortType}&sort=${sort}`)
+        setPage([page, `/api/products?page=1&provider=${provider}&sorttype=${sortType}&sort=${sort}`])
+        setLoading(false)
+        setProducts(products.data)
+      } else {
+        setLoading(true)
+        const products = await axios.get(`/api/products?page=1&title=${product}&sorttype=${sortType}&sort=${sort}`)
+        setPage([page, `/api/products?page=1&title=${product}&sorttype=${sortType}&sort=${sort}`])
+        setLoading(false)
+        setProducts(products.data)
+      }
+    }
+  }
+
+  return <>
+    <section className="search-container">
+      <h2>¡BUSCA TU PRODUCTO YA!</h2>
+
+      <form onSubmit={submitSearch} className="search-form" action="#">
+
+        <label>
+          <span>Producto</span>
+          <input onChange={changeProductInput} type="text" name="product" id="product" />
+        </label>
+
+        <label>
+          <span>Fabricante</span>
+          <input onChange={changeProviderInput} type="text" name="provider" id="provider" />
+        </label>
+
+        <label>
+          <span>Filtrar por</span>
+          <select name="sorttype" id="sorttype">
+            <option value="title">Titulo</option>
+            <option value="price">Precio</option>
+            <option value="relevance">Relevancia</option>
+          </select>
+        </label>
+
+        <label>
+          <span>Ordenar de</span>
+          <select name="sort" id="sort">
+            <option value="desc">Mayor a menor</option>
+            <option value="asc">Menor a mayor</option>
+          </select>
+        </label>
+
+        <button type="submit">BUSCAR</button>
+
+        {errorMessage.length ? <p className="error-message">{errorMessage}</p> : <p className="hide"></p>}
+
+      </form>
+    </section>
+
+    {loading ? <article className="loader">
+    <ThreeDots height="80" width="100%" radius="9" color="#3B3B3B" ariaLabel="three-dots-loading" wrapperStyle={{}} wrapperClassName="" visible={true} />
+    </article> : <></>}
+  </>
 };
 
 export default Search;
