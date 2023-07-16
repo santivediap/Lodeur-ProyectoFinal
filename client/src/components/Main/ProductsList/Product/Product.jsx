88564e7c50cc199cc0330c1720f52b18ef5f331f
santivediap <santivediap@gmail.com>
2023-07-16 19:44:06 +0200

    Change product title from h2 to anchor

--- client/src/components/Main/ProductsList/Product/Product.jsx
+++ client/src/components/Main/ProductsList/Product/Product.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Product = ({ title, image, price, relevance }) => {
   return <article className="product-container">
     <img className="product-img" src={image} alt={`${title}-img`} />
     <div className="product-info">
-      <h2>{title}</h2>
+      <Link to={`/${title}?image=${image}&price=${price}&relevance=${relevance}`}>{title}</Link>
       <p>{price.toFixed(2)}€</p>
       <img className="relevance" src={`assets/relevance-${relevance}.png`} alt="relevance-img" />
     </div>
