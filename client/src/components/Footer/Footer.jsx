d5e283ba2c35b0729060987bcd25cd9b3764d5ac
santivediap <santivediap@gmail.com>
2023-07-17 23:46:02 +0200

    Add screenshots and change footer content

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return <footer>
     <section className="footer-container">
-      <span>Footer</span>
+      <span>Contáctanos!</span>
+      <span>Telef: +34 685 837 698</span>
+      <span>Dirección: C / de la Plaza 6</span>
     </section>
   </footer>;
 };
